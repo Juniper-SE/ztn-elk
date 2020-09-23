0ff413b5821c8bdfb7143e7129fe1d155f56c2e7
Natalie Landsberg <natalie.landsberg97@gmail.com>
2020-09-23 15:51:07 -0400

    show request args

--- ztn/ztn/app.py
+++ ztn/ztn/app.py
@@ -6,10 +6,17 @@ app = Flask(__name__, template_folder="../templates")
 
 @app.route('/')
 def index():
-    srcaddr = request.args.get("srcaddr")
+    print(request.args)
+    # srcaddr = str(request.args.get("srcaddr"))
+    # srcport = str(request.args.get("srcport"))
+    # destaddr = str(request.args.get("destaddr"))
+    # destport = str(request.args.get("destport")
+    # servicename = request.args.get("servicename")
+    # srczone = request.args.get("srczone")
+    # destzone = request.args.get("destzone")
 
-    ztn_elk.create_application(servicename, destport, srcport)
-    ztn_elk.create_policy()
+    # ztn_elk.create_application(servicename, destport, srcport)
+    # ztn_elk.create_policy()
     return render_template("index.html")
 
     # return render_template("index.html")
