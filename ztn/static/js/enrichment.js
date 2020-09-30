ba1bfdffc75a7f66a0467ffdf7f3174a9cba518e
Natalie Landsberg <natalie.landsberg97@gmail.com>
2020-09-30 11:40:32 -0400

    add useRecommended function

--- ztn/static/js/enrichment.js
+++ ztn/static/js/enrichment.js
@@ -26,8 +26,28 @@ function useRecommended() {
     var isChecked = document.getElementById("recommended").checked;
 
     if (isChecked) {
-        alert("checked, yeehaw")
-    } else {
-        alert("not checked :(")
+        // Set source address to subnet + /24 cidr
+        document.getElementById("sourceaddr").selectedIndex = 1;
+        document.getElementById("src_cidr").value = 24;
+
+        // Set source port to specific port
+        document.getElementById("sourceport").selectedIndex = 0;
+
+        // Set destination address to subnet + /24 cidr
+        document.getElementById("desteaddr").selectedIndex = 1;
+        document.getElementById("dest_cidr").value = 24;
+
+        // Set dest port to specific port
+        document.getElementById("destport").selectedIndex = 0;
+
+        // Set source + dest zone to value from log
+        document.getElementById("srczone").selectedIndex = 0;
+        document.getElementById("destzone").selectedIndex = 0;
+
+        // Set username to one from log
+        document.getElementById("username").selectedIndex = 0;
+
+        // Set application to one from log
+        document.getElementById("application").selectedIndex = 0;
     }
 }
