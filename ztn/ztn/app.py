a37716ca0098df4d509fc70a72697d6bb30bb014
Natalie Landsberg <natalie.landsberg97@gmail.com>
2020-10-26 15:07:14 -0400

    import flash

--- ztn/ztn/app.py
+++ ztn/ztn/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, send_from_directory
+from flask import Flask, request, render_template, send_from_directory, flash
 import ztn_elk
 import os
 import logging
