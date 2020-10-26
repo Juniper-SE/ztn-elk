a67ef088fb8649c4b37a06dc60f2854ae9634946
Natalie Landsberg <natalie.landsberg97@gmail.com>
2020-10-26 15:06:10 -0400

    add capability to upload a file for enrichment pt1

--- ztn/ztn/app.py
+++ ztn/ztn/app.py
@@ -4,7 +4,14 @@ import os
 import logging
 import json
 import time
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = '../uploads'
+ALLOWED_EXTENSIONS = {'csv'}
+
 app = Flask(__name__, template_folder="../templates")
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
 # Logging configuration
 logging.basicConfig(filename="ztn_elk.log",
@@ -154,6 +161,23 @@ def enriched_data():
         "protocol_id": str(args['protocol_id']),
         "qs": request.query_string.decode('utf-8')
     }
+
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
 
     return render_template("enrichment.html", **content)
 
