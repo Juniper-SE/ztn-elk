c8af8952ab32d5f55088055a5c9e1abf5e981411
Natalie Landsberg <natalie.landsberg97@gmail.com>
2020-10-26 17:05:03 -0400

    remove flash import

--- ztn/ztn/app.py
+++ ztn/ztn/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, send_from_directory, flash
+from flask import Flask, request, render_template, send_from_directory
 import ztn_elk
 import os
 import logging
@@ -170,13 +170,13 @@ def enriched_data():
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
-            flash('No file part')
+            print('No file part')
             return render_template("enrichment.html", **content)
         file = request.files['file']
         # if user does not select file, browser also
         # submit an empty part without filename
         if file.filename == '':
-            flash('No selected file')
+            print('No selected file')
             return render_template("enrichment.html", **content)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
